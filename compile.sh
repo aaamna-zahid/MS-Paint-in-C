0141bb24ebecf4009aeb23786c231a38c2a2a8f7
Amna Zahid <aaamna.zahid@gmail.com>
2025-05-26 06:01:55 +0500

    Add files via upload

---
+++ compile.sh
@@ -0,0 +1,6 @@
+#1/bin\bash
+g++ -o main.exe main.c .\data\*.c .\brain\*.c .\interface\*.c
+
+
+.\main.exe
+ 
---
+++ main.c
@@ -0,0 +1,9 @@
+#include ".\data\data_header.h"
+#include ".\brain\brain_header.h"
+#include ".\interface\interface_header.h"
+
+int main()
+{
+
+    main_menu();
+}
